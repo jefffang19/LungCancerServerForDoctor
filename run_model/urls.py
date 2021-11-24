5021243a5cecd2e7f5daad281a39d060a58a2aa9
jefffang19 <jefffang07@gmail.com>
2021-11-24 16:58:45 +0800

    Add show image on template

--- run_model/urls.py
+++ run_model/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('test', views.hello, name='test'),
     path('test_template', views.hello_template, name='test_template'),
+    path('test_img', views.hello_img, name='test_img'),
 ]
