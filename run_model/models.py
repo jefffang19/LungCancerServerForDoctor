c418c14b5880e8cc9d3bb1838f0e227133573252
jefffang19 <jefffang07@gmail.com>
2021-11-30 17:57:29 +0800

    Modify case table

--- run_model/models.py
+++ run_model/models.py
@@ -10,3 +10,12 @@ class Case(models.Model):
     inner_uuid = models.CharField(max_length=50)
     file_path = models.CharField(max_length=50)
     origin_file_name = models.CharField(max_length=50)
+    file_type = models.CharField(max_length=10)
+
+
+class Modelquery(models.Model):
+    case = models.OneToOneField(
+        Case, on_delete=models.CASCADE, primary_key=True)
+    state = models.IntegerField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
