11a8718eabc090bffa63c11743e01a8bb10fb036
jefffang19 <jefffang07@gmail.com>
2021-12-03 15:04:15 +0800

    Add display probability in history page

--- run_model/controllers/history_controller.py
+++ run_model/controllers/history_controller.py
@@ -33,7 +33,13 @@ def history(request):
             nodule_prob.append(' ')
         else:
             inference_status.append('Done')
-            nodule_prob.append(' ')
+            # get probability
+            # calculate model statistic
+            prob = [mp[0].predict_prob_0, mp[0].predict_prob_1, mp[0].predict_prob_2, mp[0].predict_prob_3, mp[0].predict_prob_4, mp[0].predict_prob_5, mp[0].predict_prob_6, mp[0].predict_prob_7,
+            mp[0].predict_prob_8, mp[0].predict_prob_9, mp[0].predict_prob_10, mp[0].predict_prob_11, mp[0].predict_prob_12, mp[0].predict_prob_13, mp[0].predict_prob_14, mp[0].predict_prob_15]
+            prob = np.array(prob)
+
+            nodule_prob.append(prob.max())
 
         difficult_case.append('Unknown')
 
