36a5f23801d2066ccf7e28a79549f5337478e82c
jefffang19 <jefffang07@gmail.com>
2022-01-06 17:32:23 +0800

    fix a bug regarding gender

--- run_model/forms.py
+++ run_model/forms.py
@@ -4,9 +4,9 @@ from django import forms
 class UploadFileForm(forms.Form):
     age = forms.CharField(max_length=50)
     SEX_CHOICE = (
-        (1, ("Female")),
-        (2, ("Male")),
-        (3, ("Other")),
+        (0, ("Female")),
+        (1, ("Male")),
+        (2, ("Other")),
     )
     gender = forms.ChoiceField(choices=SEX_CHOICE)
     tumer_location = forms.CharField(max_length=200)
